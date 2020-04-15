ce9a3bc99fc275249c18a40405128cfa47e385a2
nicholasbroussard <35344009+nicholasbroussard@users.noreply.github.com>
2020-04-15 13:27:10 -0500

    Added back in city_pop_stats

--- census_data.R
+++ census_data.R
@@ -15,7 +15,7 @@ population_2015_zips <- fromJSON("C:\\Users\\nicho\\Desktop\\population_2015.jso
 pop_2015_zips <- as.data.frame(pop_2015_zips, row.names = F)
 pop_2015_zips <- pop_2015_zips[-c(1),]
 colnames(pop_2015_zips) <- c("population_zip_2015", "zip")
-write.csv(pop_2015_zips, 'population_zip_2015.csv', row.names = F)
+write.csv(pop_2015_zips, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\population_zip_2015.csv', row.names = F)
 
 #population_zip_2018
 #https://api.census.gov/data/2018/acs/acs5/profile?get=DP04_0001E&for=zip%20code%20tabulation%20area:*&key=f0ac46713fecc95dd2bd8965c8af599c248ce387
@@ -23,16 +23,16 @@ population_zip_2018 <- fromJSON("C:\\Users\\nicho\\Desktop\\profile.json", flatt
 zips <- as.data.frame(zips, row.names = F)
 zips <- zips[-c(1),]
 colnames(zips) <- c("population_zip_2018", "zip")
-write.csv(zips, 'population_zip_2018.csv', row.names = F)
+write.csv(zips, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\population_zip_2018.csv', row.names = F)
 
 #Combined populations tables
-population_zip_2015 <- read.csv('C:\\Users\\nicho\\Desktop\\population_zip_2015.csv', stringsAsFactors = F)
-population_zip_2018 <- read.csv('C:\\Users\\nicho\\Desktop\\population_zip_2018.csv', stringsAsFactors = F)
+population_zip_2015 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\population_zip_2015.csv', stringsAsFactors = F)
+population_zip_2018 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\population_zip_2018.csv', stringsAsFactors = F)
 
 population_data_zip <- full_join(population_zip_2015, population_zip_2018, by = 'zip') %>%
   select(zip, population_zip_2015, population_zip_2018)
 
-write.csv(population_data_zip, 'C:\\Users\\nicho\\Desktop\\population_data_zip.csv', row.names = F)
+write.csv(population_data_zip, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\population_data_zip.csv', row.names = F)
 
 
 #population_data_city
@@ -55,19 +55,19 @@ income_city_2018$city <- str_remove(income_city_2018$city, "town")
 income_data_city <- full_join(income_city_2015, income_city_2018, by = 'city') %>%
   select(city, income_city_2015, income_city_2018)
 
-write.csv(income_data_city, 'C:\\Users\\nicho\\Desktop\\income_data_city.csv')
+write.csv(income_data_city, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_data_city.csv')
 
 
 #income_data_county
 #Downloaded directly from data.census.gov, using topic = 'Population' and geographies = 'county' + 'Texas'.
 #Cleaned up the files from the Census zip file download using Excel. Renamed file name and column names. Removed all cols except for geography and income estimate. 
-income_county_2015 <- read.csv('C:\\Users\\nicho\\Desktop\\income_county_2015.csv', stringsAsFactors = F)
-income_county_2018 <- read.csv('C:\\Users\\nicho\\Desktop\\income_county_2018.csv', stringsAsFactors = F)
+income_county_2015 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_county_2015.csv', stringsAsFactors = F)
+income_county_2018 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_county_2018.csv', stringsAsFactors = F)
 
 income_data_county <- full_join(income_county_2015, income_county_2018, by = 'county') %>%
   select(county, income_county_2015, income_county_2018)
 
-write.csv(income_data_county, 'C:\\Users\\nicho\\Desktop\\income_data_county.csv', row.names = F)
+write.csv(income_data_county, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_data_county.csv', row.names = F)
 
 
 #income_data_zip
@@ -77,7 +77,7 @@ income_zip_2015 <- fromJSON("C:\\Users\\nicho\\Desktop\\income_zip_2015.json", f
 income_zip_2015 <- as.data.frame(income_zip_2015, row.names = F)
 income_zip_2015 <- income_zip_2015[-c(1),]
 colnames(income_zip_2015) <- c("income_zip_2015", "zip")
-write.csv(income_zip_2015, 'C:\\Users\\nicho\\Desktop\\income_zip_2015.csv', row.names = F)
+write.csv(income_zip_2015, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_zip_2015.csv', row.names = F)
 
 #income_zip_2018
 #https://api.census.gov/data/2018/acs/acs5/profile?get=DP03_0062E&for=zip%20code%20tabulation%20area:*&key=f0ac46713fecc95dd2bd8965c8af599c248ce387
@@ -85,23 +85,23 @@ income_zip_2018 <- fromJSON("C:\\Users\\nicho\\Desktop\\income_zip_2018.json", f
 income_zip_2018 <- as.data.frame(income_zip_2018, row.names = F)
 income_zip_2018 <- income_zip_2018[-c(1),]
 colnames(income_zip_2018) <- c("income_zip_2018", "zip")
-write.csv(income_zip_2018, 'C:\\Users\\nicho\\Desktop\\income_zip_2018.csv', row.names = F)
+write.csv(income_zip_2018, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_zip_2018.csv', row.names = F)
 
 #combined file
-income_zip_2015 <- read.csv('C:\\Users\\nicho\\Desktop\\income_zip_2015.csv', stringsAsFactors = F)
-income_zip_2018 <- read.csv('C:\\Users\\nicho\\Desktop\\income_zip_2018.csv', stringsAsFactors = F)
+income_zip_2015 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_zip_2015.csv', stringsAsFactors = F)
+income_zip_2018 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_zip_2018.csv', stringsAsFactors = F)
 
 income_data_zip <- full_join(income_zip_2015, income_zip_2018, by = 'zip') %>%
   select(zip, income_zip_2015, income_zip_2018)
 
-write.csv(income_data_zip, 'C:\\Users\\nicho\\Desktop\\income_data_zip.csv', row.names = F)
+write.csv(income_data_zip, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_data_zip.csv', row.names = F)
 
 
 #income_data_city
 #Downloaded directly from data.census.gov, using topic = 'Income and Earnings' and geographies = 'place' + 'Texas'.
 #Cleaned up the files from the Census zip file download using Excel. Renamed file name and column names. Removed all cols except for geography and income estimate.
-income_city_2015 <- read.csv('C:\\Users\\nicho\\Desktop\\income_city_2015.csv', stringsAsFactors = F)
-income_city_2018 <- read.csv('C:\\Users\\nicho\\Desktop\\income_city_2018.csv', stringsAsFactors = F)
+income_city_2015 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_city_2015.csv', stringsAsFactors = F)
+income_city_2018 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_city_2018.csv', stringsAsFactors = F)
 
 income_city_2015$city <- as.character(income_city_2015$city)
 income_city_2018$city <- as.character(income_city_2018$city)
@@ -117,16 +117,16 @@ income_city_2018$city <- str_remove(income_city_2018$city, "town")
 income_data_city <- full_join(income_city_2015, income_city_2018, by = 'city') %>%
   select(city, income_city_2015, income_city_2018)
 
-write.csv(income_data_city, 'C:\\Users\\nicho\\Desktop\\income_data_city.csv', row.names = F)
+write.csv(income_data_city, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_data_city.csv', row.names = F)
 
 
 #income_data_county
 #Downloaded directly from data.census.gov, using topic = 'Income and Earnings' and geographies = 'county' + 'Texas'.
 #Cleaned up the files from the Census zip file download using Excel. Renamed file name and column names. Removed all cols except for geography and income estimate. 
-income_county_2015 <- read.csv('C:\\Users\\nicho\\Desktop\\income_county_2015.csv', stringsAsFactors = F)
-income_county_2018 <- read.csv('C:\\Users\\nicho\\Desktop\\income_county_2018.csv', stringsAsFactors = F)
+income_county_2015 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_county_2015.csv', stringsAsFactors = F)
+income_county_2018 <- read.csv('C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_county_2018.csv', stringsAsFactors = F)
 
 income_data_county <- full_join(income_county_2015, income_county_2018, by = 'county') %>%
   select(county, income_county_2015, income_county_2018)
 
-write.csv(income_data_county, 'C:\\Users\\nicho\\Desktop\\income_data_county.csv', row.names = F)
+write.csv(income_data_county, 'C:\\Users\\nicho\\OneDrive\\Nick\\Data & Math\\Useful Files\\income_data_county.csv', row.names = F)
